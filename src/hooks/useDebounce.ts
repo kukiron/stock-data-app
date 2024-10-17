91e72c1e654ab47b7d92d3d0c2fb1cf1be47e3ed
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2024-10-17 21:50:57 +0600

    custom debounce hook

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,33 @@
+import debounce from 'lodash/debounce';
+import { useRef, useEffect, useMemo } from 'react';
+
+/**
+ * @param callback: the callback function to be debounced
+ * @param timeout: the timeout value for the debounce function
+ * @returns debouncedCallback: the debounced callback function
+ */
+
+// hook to safely use lodash debounce
+// to avoid cases where debounce becomes essentially just a delayed function call
+const useDebounce = (
+  callback: (...args: any) => any,
+  timeout: number = 500
+) => {
+  const ref = useRef<any>();
+
+  useEffect(() => {
+    ref.current = callback;
+  }, [callback]);
+
+  const debouncedCallback = useMemo(() => {
+    const func = (...args: any): any => {
+      ref.current?.(...args);
+    };
+
+    return debounce(func, timeout);
+  }, [timeout]);
+
+  return debouncedCallback;
+};
+
+export default useDebounce;

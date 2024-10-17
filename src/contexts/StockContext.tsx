f7f0cc16f95048c67e3bbce65b1921508450d342
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2024-10-17 23:29:47 +0600

    update context level state

--- src/contexts/StockContext.tsx
+++ src/contexts/StockContext.tsx
@@ -1,26 +1,50 @@
-import { createContext, useState } from 'react';
-
-import { SearchedItem } from 'data/types';
+import { createContext, Dispatch, useReducer } from 'react';
 
 import NewsFeed from 'components/News';
 import CompanyOverview from 'components/Overview';
 import Searchbar from 'components/Searchbar';
+import { ApiLimitCard } from 'components/common';
+
+import { appStateReducer } from 'data/reducers';
+import type { Action, AppState } from 'data/types';
 
 type ContextState = {
-  activeData: SearchedItem | null;
-  setActiveData: (stock: SearchedItem | null) => void;
+  appState: AppState;
+  updateAppState: Dispatch<Action>;
+};
+
+const initialAppState = {
+  errorMessage: '',
+  activeData: null,
 };
 
 export const StockContext = createContext({} as ContextState);
 
+// Company overview & news data are fetched based on searched result
+// if no searhced result, the following endpoint calls are skipped
 export default function CompanyStock() {
-  const [activeData, setActiveData] = useState<SearchedItem | null>(null);
+  const [appState, updateAppState] = useReducer(
+    appStateReducer,
+    initialAppState
+  );
+  const { errorMessage, activeData } = appState;
+
+  const renderAppContent = () => {
+    if (errorMessage && !activeData) {
+      return <ApiLimitCard />;
+    }
+    return (
+      <>
+        <CompanyOverview />
+        <NewsFeed />
+      </>
+    );
+  };
 
   return (
-    <StockContext.Provider value={{ activeData, setActiveData }}>
+    <StockContext.Provider value={{ appState, updateAppState }}>
       <Searchbar />
-      <CompanyOverview />
-      <NewsFeed />
+      {renderAppContent()}
     </StockContext.Provider>
   );
 }
